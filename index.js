f7f508ce4dd7f7912f555bd10573c6c7954aff45
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-08-04 13:40:31 +0530

    inial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+//create a function that find element inside the array if it is not able to find the array then retur the -1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let userInput = Number(prompt("Enter a number to search:"));
+// function searchArray(arr, x) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === x) {
+//       return console.log(`the index  number is ${i} and the number is ${arr[i]}`);
+//     }
+//   }
+//   return -1;
+// }
+
+// const result = searchArray(arr, userInput);
+// console.log(result);
+
+// ----------------------------------------------------------
+
+// write a function that count number of neg
+
+// const arr = [1, 2, 3, 4, -5, -6, -7, -8];
+
+// let userInput = Number(prompt("Enter your Number here"))
+
+// function countNeg(arr) {
+//   let counter = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+
+// let result = countNeg(arr);
+// console.log(result);
+
+// --------------------------------------------------------------
+
+//find the largest number inside in your array
+
+// let arr = [12, 34, 13, 56, 7, 8, 45];
+
+// let largest = -Infinity;
+
+// function largestNumber(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > largest) largest = arr[i];
+//   }
+//  return  largest;
+// }
+
+// let result = largestNumber(arr);
+// console.log(result)
+
+// find the smallest number in your array
+
+let arr = [10, 2, 3, 4, 5, 6, 67, 889];
+
+let smallest = arr[0];
+
+function smallestNumber(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (smallest > arr[i]) {
+      smallest = arr[i];
+    }
+  }
+  return smallest
+}
+
+
+let result = smallestNumber(arr)
+console.log(result)
