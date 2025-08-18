0a58b9926280b308deb941e913a66bb6eff444fc
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-08-18 15:59:28 +0530

    number pattern printed

--- star_pattern/index.js
+++ star_pattern/index.js
@@ -1,5 +1,3 @@
-
-
 // star pattern
 
 // *****
@@ -17,20 +15,41 @@
 //     console.log(row)
 // }
 
-
-
 // second patter
 // *
 // **
 // ***
 // ****
- let n =5;
+//  let n =5;
 
- for (let i = 0; i < n-1; i++) {
-    let row =""
-    for (let j = 0; j < i+1; j++) {
-        row = row + "*"
-    }
-    console.log(row)
-    
- }
+//  for (let i = 0; i < n-1; i++) {
+//     let row =""
+//     for (let j = 0; j < i+1; j++) {
+//         row = row + "*"
+//     }
+//     console.log(row)
+
+//  }
+
+// 1
+// 12
+// 123
+// 1234
+
+// let n = 5;
+// for (let i = 1; i <= 4; i++) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row = row + (j + 1);
+//   }
+//   console.log(row);
+// }
+
+let n = 5;
+for (let i = 1; i <= n ; i++) {
+  let row = "";
+  for (let j = 0; j < i; j++) {
+    row = row + (i);
+  }
+  console.log(row);
+}
