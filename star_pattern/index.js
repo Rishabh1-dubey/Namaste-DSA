3d7292901094602b80c2c45a0814c3c5d45f3719
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-08-18 16:43:25 +0530

    number pattern printed

--- star_pattern/index.js
+++ star_pattern/index.js
@@ -9,7 +9,7 @@
 // for (let i = 0; i <n-1; i++) {
 
 //     let row =""
-//     for (let j = 0; j <n; j++) {
+//     for (let j = 1; j <n; j++) {
 //       row = row + "*"
 //     }
 //     console.log(row)
@@ -45,11 +45,49 @@
 //   console.log(row);
 // }
 
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// let n = 5;
+// for (let i = 1; i <= n ; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row = row +i;
+//   }
+//   console.log(row);
+// }
+
+// let n = 6;
+// for (let i =0; i < n - 1; i++) {
+//   let row = "";
+//   for (let j =1; j < n - i; j++) {
+//     row = row + j;
+//   }
+//   console.log(row);
+// }
+
+
+
+
+// 1
+// 10
+// 101
+// 1010
+// 10101
 let n = 5;
-for (let i = 1; i <= n ; i++) {
+
+for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = 0; j < i; j++) {
-    row = row + (i);
-  }
+  let switchh = 1;
+  for (let j = 0; j < i + 1; j++) {
+    row = row + switchh;
+    if (switchh == 1) {
+        switchh = 0;
+    } else {
+        switchh = 1;
+    }
+}
   console.log(row);
 }
