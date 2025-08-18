54b45662a8e8342e26f0333b6a1c93b80701f23c
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-08-18 15:30:20 +0530

    first pattern is completed

---
+++ star_pattern/index.js
@@ -0,0 +1,19 @@
+
+
+// star pattern
+
+// *****
+// *****
+// *****
+// *****
+let n=5;
+
+for (let i = 0; i <n-1; i++) {
+
+    let row =""
+    for (let j = 0; j <n; j++) {
+      row = row + "*"
+    }
+    console.log(row)
+}
+
